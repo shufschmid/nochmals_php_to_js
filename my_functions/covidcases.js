5aff8cd2bde0adb45785340c0a8856fc0d268ab1
shufschmid <shufschmid@gmail.com>
2020-12-29 23:13:41 +0100

    echte API BS und BL, Datenverarbeitung

--- my_functions/covidcases.js
+++ my_functions/covidcases.js
@@ -1,13 +1,42 @@
 const fetch = require('node-fetch')
 
 const API_ENDPOINT = "https://data.bs.ch/api/records/1.0/search/?dataset=100077&q=BS+or+BL&rows=100&sort=update";
+
 let ausgabe = ""
+const allBS = []
+const allBL = []
 
 exports.handler = async (event, context) => {
-    return fetch('https://data.bs.ch/api/records/1.0/search/?dataset=100073&q=&sort=timestamp&facet=timestamp')
+  return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
     .then(response => response.json())
-    .then(function(data){
-        data.records.forEach(post => { console.log(post.fields.current_quarantined_total)});
+    .then(function (data) {
+      // Displaying it to the DOM
+      data.records.forEach(post => {
+        if (post.fields.abbreviation_canton_and_fl == "BS") {
+          allBS.push(parseInt(post.fields.ncumul_conf));
+        }
+        else if (post.fields.abbreviation_canton_and_fl == "BL") {
+          allBL.push(parseInt(post.fields.ncumul_conf));
+        }
+      });
+      allBS.reverse()
+      allBL.reverse()
+      let newCasesCumBS = []
+      let newCasesCumBL = []
+      allBS.forEach((e, index) => {
+        newCasesCumBS.push(index > 0 ? e - allBS[index - 1] > 0 ? e - allBS[index - 1] : 0 : 0);
+
+      })
+
+      allBL.forEach((e, index) => {
+        newCasesCumBL.push(index > 0 ? e - allBL[index - 1] > 0 ? e - allBL[index - 1] : 0 : 0)
+      })
+      ausgabe = JSON.stringify(newCasesCumBL)
+
     })
-    
-   }; 
+    .then(data => ({
+      statusCode: 200,
+      body: ausgabe
+    }))
+    .catch(error => ({ statusCode: 422, body: String(error) }));
+};
