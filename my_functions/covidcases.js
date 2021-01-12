a2fc55a0b8fac013c26a410df571af8c216da571
shufschmid <shufschmid@gmail.com>
2021-01-12 15:14:28 +0100

    add of inzidenz bs

--- my_functions/covidcases.js
+++ my_functions/covidcases.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch')
 
-const API_ENDPOINT = "https://data.bs.ch/api/records/1.0/search/?dataset=100077&q=BS+or+BL&rows=100&sort=update";
+const API_ENDPOINT = "https://data.bs.ch/api/records/1.0/search/?dataset=100108&q=&rows=100&sort=test_datum&facet=test_datum";
 
 
 
@@ -8,39 +8,34 @@ exports.handler = async (event, context) => {
 let ausgabe = ""
 const allBS = []
 const allBL = []
+
 const infogramSchema = [[]] // Schema-Beschreibung https://infogram.com/api/examples/live.json
   return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
     .then(response => response.json())
     .then(function (data) {
-      // Displaying it to the DOM
       data.records.forEach(post => {
-        if (post.fields.abbreviation_canton_and_fl == "BS") {
-          allBS.push(parseInt(post.fields.ncumul_conf));
-        }
-        else if (post.fields.abbreviation_canton_and_fl == "BL") {
-          allBL.push(parseInt(post.fields.ncumul_conf));
-        }
+        
+        allBS.push(post.fields.test_datum,parseInt(post.fields.faelle_bs),parseInt(post.fields.inzidenz14_bs));
+   
       });
       allBS.reverse()
       allBL.reverse()
-      let newCasesCumBS = []
       let newCasesCumBL = []
-      allBS.forEach((e, index) => {
+      /*allBS.forEach((e, index) => {
         newCasesCumBS.push(index > 0 ? e - allBS[index - 1] > 0 ? e - allBS[index - 1] : 0 : 0);
 
-      })
+      })*/
 
-      allBL.forEach((e, index) => {
+      /*allBL.forEach((e, index) => {
         newCasesCumBL.push(index > 0 ? e - allBL[index - 1] > 0 ? e - allBL[index - 1] : 0 : 0)
-      })
+      })*/
       infogramSchema[0].push([
         "Corona Infektionen",
-        "Basel-Stadt",
-        "Baselland",
+        "neue Fälle",
+        "14-Tage-Inzidenz",
     ])
     
-    infogramSchema[0].push(newCasesCumBS)
-    infogramSchema[0].push(newCasesCumBL)
+    infogramSchema[0].push(allBS)
     ausgabe = JSON.stringify(infogramSchema)
 
     })
