8897421f2ab4172395c7d446ce650499e975fa52
shufschmid <shufschmid@gmail.com>
2021-01-12 15:25:34 +0100

    simplified

--- my_functions/covidcases.js
+++ my_functions/covidcases.js
@@ -10,15 +10,16 @@ const allBS = []
 const allBL = []
 
 const infogramSchema = [[]] // Schema-Beschreibung https://infogram.com/api/examples/live.json
+
   return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
     .then(response => response.json())
     .then(function (data) {
       data.records.forEach(post => {
         
-        allBS.push(post.fields.test_datum,parseInt(post.fields.faelle_bs),parseInt(post.fields.inzidenz14_bs));
+        infogramSchema[0].push([post.fields.test_datum,parseInt(post.fields.faelle_bs),parseInt(post.fields.inzidenz14_bs)]);
    
       });
-      allBS.reverse()
+      //allBS.reverse()
       allBL.reverse()
       let newCasesCumBL = []
       /*allBS.forEach((e, index) => {
@@ -29,13 +30,15 @@ const infogramSchema = [[]] // Schema-Beschreibung https://infogram.com/api/exam
       /*allBL.forEach((e, index) => {
         newCasesCumBL.push(index > 0 ? e - allBL[index - 1] > 0 ? e - allBL[index - 1] : 0 : 0)
       })*/
-      infogramSchema[0].push([
-        "Corona Infektionen",
-        "neue Fälle",
-        "14-Tage-Inzidenz",
-    ])
+      
     
-    infogramSchema[0].push(allBS)
+    //infogramSchema[0].push(allBS)
+    infogramSchema[0].push([
+      "Corona Infektionen",
+      "neue Fälle",
+      "14-Tage-Inzidenz",
+    ])
+    infogramSchema[0].reverse()
     ausgabe = JSON.stringify(infogramSchema)
 
     })
