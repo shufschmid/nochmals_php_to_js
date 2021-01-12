dfc1d9c4a099ef21865031f3af48c4d3fd9e74dd
shufschmid <shufschmid@gmail.com>
2021-01-12 22:55:22 +0100

    now cleaner and with parameter anzahl=100 in url

--- my_functions/covidcases.js
+++ my_functions/covidcases.js
@@ -1,50 +1,29 @@
-const fetch = require('node-fetch')
-
-const API_ENDPOINT = "https://data.bs.ch/api/records/1.0/search/?dataset=100108&q=&rows=100&sort=test_datum&facet=test_datum";
-
-
-
 exports.handler = async (event, context) => {
-let ausgabe = ""
-const allBS = []
-const allBL = []
-
-const infogramSchema = [[]] // Schema-Beschreibung https://infogram.com/api/examples/live.json
+  const fetch = require('node-fetch') //diese Bibliothek wird gebraucht, damit man wie bei normalem Javascript fetch verwenden kann
+  const anzahl = event.queryStringParameters.anzahl   //👉https://flaviocopes.com/netlify-functions-query-parameters/
+  const API_ENDPOINT = "https://data.bs.ch/api/records/1.0/search/?dataset=100108&q=&rows="+anzahl+"&sort=test_datum&facet=test_datum";
+  const infogramSchema = [[]]       //👉Schema-Beschreibung https://infogram.com/api/examples/live.json
 
   return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
-    .then(response => response.json())
-    .then(function (data) {
-      data.records.forEach(post => {
-        
-        infogramSchema[0].push([post.fields.test_datum,parseInt(post.fields.faelle_bs),parseInt(post.fields.inzidenz14_bs)]);
-   
-      });
-      //allBS.reverse()
-      allBL.reverse()
-      let newCasesCumBL = []
-      /*allBS.forEach((e, index) => {
-        newCasesCumBS.push(index > 0 ? e - allBS[index - 1] > 0 ? e - allBS[index - 1] : 0 : 0);
-
-      })*/
-
-      /*allBL.forEach((e, index) => {
-        newCasesCumBL.push(index > 0 ? e - allBL[index - 1] > 0 ? e - allBL[index - 1] : 0 : 0)
-      })*/
-      
+    .then(response => response.json()) // = mach was mit der Antwort und zwar mach sie zu einem json-objekt
     
-    //infogramSchema[0].push(allBS)
-    infogramSchema[0].push([
-      "Corona Infektionen",
-      "neue Fälle",
-      "14-Tage-Inzidenz",
-    ])
-    infogramSchema[0].reverse()
-    ausgabe = JSON.stringify(infogramSchema)
+    .then(data => {                 // = mach was mit der Antwort (= dem json-objekt mit allen einträgen)
+        data.records.forEach(       // für alle einträge...
+          eintrag => {              // nimm den aktuellen eintrag..
+            infogramSchema[0].push( //und füge ihn zuhinterst im finalen Array an und zwar als 👇 "Datum", Zahl, Zahl 
+              [eintrag.fields.test_datum, parseInt(eintrag.fields.faelle_bs), parseInt(eintrag.fields.inzidenz14_bs)]
+            )
+          }
+        )
+        infogramSchema[0].push(     //wenn alle Einträge durch, füge die Spaltenüberschriften hinzu
+          ["Corona Infektionen","neue Fälle","14-Tage-Inzidenz"]
+        );
+        infogramSchema[0].reverse(); //und drehe das ganze array um, wobei das Schema bei inforam gefühlt eine eckige Klammer zu viel hat deshalb jeweils [0]
+      }    
+    )
 
-    })
-    .then(data => ({
-      statusCode: 200,
-      body: ausgabe
+    .then(ausgabe => ({             //wenn bis hierhin kein fehler: gib das Infogram-Array als String aus. dieses letzte then sowie das "catch" bezieht sich auf return ganz am Anfang der Funktion, siehe https://docs.netlify.com/functions/build-with-javascript/
+      statusCode: 200, body: JSON.stringify(infogramSchema)      
     }))
-    .catch(error => ({ statusCode: 422, body: String(error) }));
-};
+    .catch(error => ({ statusCode: 422, body: String(error) })); //sonst gib einen Fehler zurück 
+}
